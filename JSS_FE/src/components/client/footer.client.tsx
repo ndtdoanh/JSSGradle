3140eb5b982cc2eb96c34eb680afde0193086186
doanh <nguyentrongdoanh202@gmail.com>
2024-12-26 17:41:59 +0700

    update

---
+++ JSS_FE/src/components/client/footer.client.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ padding: 15, textAlign: 'center' }}>
+      {/* <div>Frontend React Typescript - Series Nest.JS Basic &copy; ndtdoanh</div> */}
+    </footer>
+  );
+};
+
+export default Footer;
